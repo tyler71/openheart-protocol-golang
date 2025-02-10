e409c897a010420a88640d0fa6d860bd89bb14bb
Tyler <4926565+tyler71@users.noreply.github.com>
2025-02-10 15:04:06 -0800

    Able to detect the first rune & emoji. The spec just says the first emoji so..

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"github.com/dmolesUC/emoji"
+	"io"
 	"net/http"
 
 	"openheart.tylery.com/internal/response"
@@ -51,14 +54,49 @@ func (app *application) getOne(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+var Url int
+
 // Increment the count for a specific emoji by 1
 func (app *application) create(w http.ResponseWriter, r *http.Request) {
-	url, emoji := r.PathValue("url"), r.PathValue("emoji")
-	fmt.Println(url, emoji)
-
-	w.WriteHeader(201)
-	_, err := w.Write([]byte("OK"))
-	if err != nil {
+	reader := bufio.NewReader(io.LimitReader(r.Body, 64))
+	emojiRune, emojiRuneSize, err := reader.ReadRune()
+	if err != nil || emojiRuneSize == 0 {
 		app.serverError(w, r, err)
 	}
+	url := r.PathValue("url")
+	fmt.Println(emojiRune, url, string(emojiRune))
+	fmt.Println(emoji.IsEmoji(emojiRune))
+	return
+	//var urlId int
+	//var emojiRecord struct {
+	//	id     int
+	//	siteId int
+	//	emoji  string
+	//	count  int
+	//}
+	//
+	//// First, we get the site id based on the url. We should probably try to parse the url to try and only get
+	//// relevant data.
+	//err = app.db.Get(&urlId, "SELECT id FROM site WHERE url=$1", url)
+	//if err != nil || urlId == 0 {
+	//	app.serverError(w, r, err)
+	//}
+	//
+	//// Next, we get the emoji count for this specific url. It might not exist! We need to check for this and create a new one
+	//// if this is the case
+	//err = app.db.Get(&emojiRecord, "SELECT id, site_id, emoji, count FROM emoji WHERE site_id=$1 AND emoji=$2", urlId, emoji)
+	//if err != nil || urlId == 0 {
+	//	app.serverError(w, r, err)
+	//}
+	//
+	//// With an existing, we need to update the value by incrementing by one. If it doesn't exist, creating the record
+	//// starts the count at 1, so we're good.
+	//tx := app.db.MustBegin()
+	//fmt.Println(url, emoji)
+	//
+	//w.WriteHeader(201)
+	//_, err := w.Write([]byte("OK"))
+	//if err != nil {
+	//	app.serverError(w, r, err)
+	//}
 }
