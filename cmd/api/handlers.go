bd8cad6658d1f64f7161ff365769f97795d96ce5
Tyler <4926565+tyler71@users.noreply.github.com>
2025-02-10 22:32:49 -0800

    Able to create new records

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"database/sql"
+	"errors"
 	"fmt"
 	"github.com/dmolesUC/emoji"
 	"io"
@@ -66,37 +68,64 @@ func (app *application) create(w http.ResponseWriter, r *http.Request) {
 	url := r.PathValue("url")
 	fmt.Println(emojiRune, url, string(emojiRune))
 	fmt.Println(emoji.IsEmoji(emojiRune))
-	return
-	//var urlId int
-	//var emojiRecord struct {
-	//	id     int
-	//	siteId int
-	//	emoji  string
-	//	count  int
-	//}
-	//
-	//// First, we get the site id based on the url. We should probably try to parse the url to try and only get
-	//// relevant data.
-	//err = app.db.Get(&urlId, "SELECT id FROM site WHERE url=$1", url)
-	//if err != nil || urlId == 0 {
-	//	app.serverError(w, r, err)
-	//}
-	//
-	//// Next, we get the emoji count for this specific url. It might not exist! We need to check for this and create a new one
-	//// if this is the case
-	//err = app.db.Get(&emojiRecord, "SELECT id, site_id, emoji, count FROM emoji WHERE site_id=$1 AND emoji=$2", urlId, emoji)
-	//if err != nil || urlId == 0 {
-	//	app.serverError(w, r, err)
-	//}
-	//
-	//// With an existing, we need to update the value by incrementing by one. If it doesn't exist, creating the record
-	//// starts the count at 1, so we're good.
-	//tx := app.db.MustBegin()
-	//fmt.Println(url, emoji)
-	//
-	//w.WriteHeader(201)
-	//_, err := w.Write([]byte("OK"))
-	//if err != nil {
-	//	app.serverError(w, r, err)
-	//}
+	var urlId int
+	var emojiRecord struct {
+		Id     int    `db:"id"`
+		SiteId int    `db:"site_id"`
+		Emoji  string `db:"emoji"`
+		Count  int    `db:"count"`
+	}
+
+	// First, we get the site id based on the url. We should probably try to parse the url to try and only get
+	// relevant data.
+	err = app.db.Get(&urlId, "SELECT id FROM site WHERE url=?", url)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		app.serverError(w, r, err)
+	}
+	var noSiteRecord bool
+	var noEmojiRecord bool
+	if urlId == 0 {
+		noSiteRecord = true
+		noEmojiRecord = true
+	} else {
+		// Next, we get the emoji count for this specific url. It might not exist! We need to check for this and create a new one
+		// if this is the case
+		err = app.db.Get(&emojiRecord, "SELECT id, site_id, emoji, count FROM emoji WHERE site_id=? AND emoji=?", urlId, emojiRune)
+		if err != nil && !errors.Is(err, sql.ErrNoRows) {
+			app.serverError(w, r, err)
+		}
+		if emojiRecord.Id == 0 {
+			noEmojiRecord = true
+		}
+	}
+
+	// With an existing, we need to update the value by incrementing by one. If it doesn't exist, creating the record
+	// starts the count at 1, so we're good.
+	tx := app.db.MustBegin()
+	defer tx.Rollback()
+	if noSiteRecord {
+		tx.MustExec("INSERT INTO site (url) VALUES (?)", url)
+		err := tx.Get(&urlId, "SELECT id FROM site WHERE url=?", url)
+		if err != nil && !errors.Is(err, sql.ErrNoRows) {
+			app.serverError(w, r, err)
+		}
+	}
+	if noEmojiRecord {
+		tx.MustExec("INSERT INTO emoji (site_id, emoji) VALUES (?, ?)", urlId, emojiRune)
+	} else {
+		tx.MustExec("UPDATE emoji SET count=? WHERE id=?", emojiRecord.Count+1, emojiRecord.Id)
+	}
+
+	tx.Commit()
+	fmt.Println(url, emojiRecord.Emoji)
+
+	if noEmojiRecord == true || noSiteRecord == true {
+		w.WriteHeader(201)
+	} else {
+		w.WriteHeader(200)
+	}
+	_, err = w.Write([]byte("OK"))
+	if err != nil {
+		app.serverError(w, r, err)
+	}
 }
